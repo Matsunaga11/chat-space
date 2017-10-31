3e2ebf06157726d7a8978d3b7d78e5d7e02911e6
takahisa matsunaga <ttaaak11@gmail.com>
2017-10-31 21:35:44 +0900

    Update 条件分岐

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,14 @@ class UsersController < ApplicationController
 
   def update
     user = User.find(params[:id])
-    user.update(tweet_params)
-  end
+   if user.update(tweet_params)
+   render "messages/index"
+   else render :edit
+   end
 end
-
 
  private
   def tweet_params
      params.permit(:name, :email)
   end
-
-
-
+end
