438ad257a8943f1aab6ac4fec05d0c2c14acbae1
takahisa matsunaga <ttaaak11@gmail.com>
2017-11-01 15:59:38 +0900

    フラッシュメッセージ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  def after_sign_out_path_for(resource)
-    '/users/sign_in'
-  end
+  # def after_sign_out_path_for(resource)
+  #   #'/users/sign_in'
+  #   #flash[:alert] = "ログインまたは登録が必要です。"
+  # end
+
+
+# flash[:notice] = "アカウントが登録されました。"
+# flash[:notice] = "ログインしました。"
 
 end
