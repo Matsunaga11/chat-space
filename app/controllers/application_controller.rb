00365540dc0e67684807e642bed886b9d1710264
takahisa matsunaga <ttaaak11@gmail.com>
2017-11-01 16:05:27 +0900

    修正
    不要なコメント、空行削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,4 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  # def after_sign_out_path_for(resource)
-  #   #'/users/sign_in'
-  #   #flash[:alert] = "ログインまたは登録が必要です。"
-  # end
-
-
-# flash[:notice] = "アカウントが登録されました。"
-# flash[:notice] = "ログインしました。"
-
 end
