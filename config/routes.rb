ff0066fc1fc405323c24bb81a37da41ccc91b351
takahisa matsunaga <ttaaak11@gmail.com>
2017-11-03 17:17:46 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 devise_for :users
 resources :users, only: [:edit, :update]
 root 'groups#index'
-resources :groups, only:[:index,:new, :edit, :create,:update] do
+resources :groups, only:[:index, :new, :edit, :create, :update] do
 end
  end
 
