bc793b071a7f62c984a86ce903beefc6a366837d
takahisa matsunaga <ttaaak11@gmail.com>
2017-11-08 20:04:44 +0900

    空行修正&ビューの細かい部分
    ・空行削除&form_forに書いていた　type属性を削除
    ・ログインしているユーザーの名前がサイドバーに表示されるように変更
    ・アクセスしているグループの名前、そのグループに属しているユーザーが表示さ　れるように変更
    ・左サイドバーのグループ名のの下にそのグループ内での最新メッセージが表示さ　れるように変更

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,2 @@
 module ApplicationHelper
-
-  def member_get(group)
-  group.users.each do |member|
-   return member.name
-  end
- end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -8,6 +8,14 @@ mount_uploader :image, ImageUploader
     image.presence || body.presence
   end
 
-
+  def self.hot_message(group)
+     hot_message = group.messages
+        if hot_message.last.try(:body) && hot_message.last.try(:body) != ""
+          hot_message.last.body
+        elsif hot_message.last.try(:image)
+           "画像を送信しました。"
+        else
+          "メッセージは送信されていません"
+        end
+  end
 end
-
