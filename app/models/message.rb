74dd48afa6bab6e45896f5d02aa28a7c7f4dc1a4
takahisa matsunaga <ttaaak11@gmail.com>
2017-11-10 11:59:40 +0900

    修正

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,14 +7,5 @@ mount_uploader :image, ImageUploader
   def image_data
     image.presence || body.presence
   end
-
-  def self.hot_message(group)
-        if group.messages.blank?
-          "メッセージは送信されていません"
-        elsif group.messages.last.body.blank?
-          "画像を投稿しました"
-        else
-          group.messages.last.body
-        end
-  end
 end
+
