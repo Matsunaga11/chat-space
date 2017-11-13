9e0aa6a9a865095a997c8af6c3342e7546b9e79e
takahisa matsunaga <ttaaak11@gmail.com>
2017-11-13 18:27:45 +0900

    require 'faker'

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,5 +1,5 @@
 FactoryGirl.define do
-
+require 'faker'
   factory :message do
     body  {Faker::Lorem.sentence}
     image   Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixture/68.jpg'))
